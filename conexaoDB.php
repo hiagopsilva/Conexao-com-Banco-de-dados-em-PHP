446e230959477c785fc999338cc8b8ce17ecaac0
Hiago Prates <hiagohps360@gmail.com>
2019-04-03 00:33:33 -0300

    Conexao com o banco de dados

---
+++ conexaoDB.php
@@ -0,0 +1,13 @@
+<?php 
+    // Passo 1 - ABRIR CONEXÃO 
+    $servidor   = "INSIRA AQUI O SEU SERVIDOR (EX: LOCALHOST)";
+    $usuario    = "INSIRA AQUI O SEU USUARIO DO SERVIDOR";
+    $senha      = "INSIRA AQUI A SUA SENHA DO SERVIDOR, SE NÃO HOUVER SENHA DEIXE EM BRANCO";
+    $banco      = "INSIRA AQUI O NOME DO BANCO";
+    $conecta    = mysqli_connect("SERVIDOR","USUARIO","SENHA","NOME DO BANCO");
+
+    //Passo 2 - TESTAR CONEXAO
+    if(mysqli_connect_errno() ){
+        die("Conexao falhou: " . mysqli_connect_errno());
+    }
+?>
