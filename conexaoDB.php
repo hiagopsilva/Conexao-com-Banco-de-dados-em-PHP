232a8b09020c14081947c5d949059969abf37f9f
Hiago Prates <47197695+hiagopsilva@users.noreply.github.com>
2019-05-12 20:54:51 -0300

    Update conexaoDB.php

--- conexaoDB.php
+++ conexaoDB.php
@@ -4,7 +4,7 @@
     $usuario    = "INSIRA AQUI O SEU USUARIO DO SERVIDOR";
     $senha      = "INSIRA AQUI A SUA SENHA DO SERVIDOR, SE NÃO HOUVER SENHA DEIXE EM BRANCO";
     $banco      = "INSIRA AQUI O NOME DO BANCO";
-    $conecta    = mysqli_connect("SERVIDOR","USUARIO","SENHA","NOME DO BANCO");
+    $conecta    = mysqli_connect($servidor,$usuario,$senha,$banco);
 
     //Passo 2 - TESTAR CONEXAO
     if(mysqli_connect_errno() ){
